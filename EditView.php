460c92bcff293e668f2e459069fded0d985c7ede
easyhorpak <easyhorpak@hotmail.com>
2020-09-08 09:49:14 +0700

    Create EditView.php

---
+++ EditView.php
@@ -0,0 +1,11 @@
+<?php
+			
+			foreach($edit_form as $row)
+                    { 
+                    
+                    	echo $row;
+                    	
+                	 	}
+    	   
+		    ?>
+        
